15e62b06621e1e53afef76af3766b101fbc408b7
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-27 16:44:15 +0200

    Added jdtls for Java development

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,29 @@
+-- This disables the annoying constant messages sent by jdtls with every
+-- keystroke.
+return {
+  "folke/noice.nvim",
+  opts = {
+    routes = {
+      {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+          cond = function(message)
+            local client = vim.tbl_get(message.opts, "progress", "client")
+            if client ~= "jdtls" then
+              return false
+            end
+
+            local content = vim.tbl_get(message.opts, "progress", "message")
+            if content == nil then
+              return false
+            end
+
+            return string.find(content, "Validate") or string.find(content, "Publish")
+          end,
+        },
+        opts = { skip = true },
+      },
+    },
+  },
+}
